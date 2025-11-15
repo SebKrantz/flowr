2c89168dcddc1c06a401fb68760d12deed845e01
SebKrantz <sebastian.krantz@graduateinstitute.ch>
2025-11-15 15:49:33 -0500

    Add counts to return.extra docs.

--- R/assignment.R
+++ R/assignment.R
@@ -16,7 +16,7 @@
 #' @param detour.max Numeric (default: 1.5). Maximum detour factor for alternative routes (applied to shortest paths cost). This is a key parameter controlling the execution time of the algorithm: considering more routes (higher \code{detour.max}) substantially increases computation time.
 #' @param angle.max Numeric (default: 90). Maximum detour angle (in degrees, two sided). I.e., nodes not within this angle measured against a straight line from origin to destination node will not be considered for detours.
 #' @param return.extra Character vector specifying additional results to return. Options include:
-#'   \code{"graph"}, \code{"dmat"}, \code{"paths"} (most memory intensive), \code{"edges"}, \code{"costs"}, \code{"weights"}.
+#'   \code{"graph"}, \code{"dmat"}, \code{"paths"} (most memory intensive), \code{"edges"}, \code{"counts"}, \code{"costs"}, and \code{"weights"}.
 #'   Use \code{"all"} to return all available extra results.
 #' @param verbose Show progress bar?
 #'
